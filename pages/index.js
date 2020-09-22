81676db373fba3017b91ca69cc25bbb8952878fe
Riaz <rhassan70@gmail.com>
2020-09-22 14:59:41 +0500

    From the .Code

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>…</Head>
-      <section className={utilStyles.headingMd}> One two three </section>
+      <section className={utilStyles.headingMd}> From the .Code </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
