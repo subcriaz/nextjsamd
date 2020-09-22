b957b894f785ee23d8df52d4ed1cca1445e3c0de
Riaz <rhassan70@gmail.com>
2020-09-22 15:17:23 +0500

    ok

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>…</Head>
-      <section className={utilStyles.headingMd}> pushed too ... </section>
+      <section className={utilStyles.headingMd}> pushed too ... ok </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
